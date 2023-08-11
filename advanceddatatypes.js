692ab390524e5fe67f76fda48a8085871dfe9f89
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-08-11 20:52:02 +0530

    advancetypedatatype

--- advanceddatatypes.js
+++ advanceddatatypes.js
@@ -177,3 +177,60 @@
 // }
 
 
+/**
+ * 
+ * Map and Set
+ * 
+ * Map
+ * new Map() – creates the map.
+ * map.set(key, value) – stores the value by the key.
+ * map.get(key) – returns the value by the key, undefined if key doesn’t exist in map.
+ * map.has(key) – returns true if the key exists, false otherwise.
+ * map.delete(key) – removes the value by the key.
+ * map.clear() – removes everything from the map.
+ * map.size – returns the current element count.
+ * 
+ * Iteration over Map
+ * map.keys() – returns an iterable for keys,
+ * map.values() – returns an iterable for values,
+ * map.entries() – returns an iterable for entries [key, value], it’s used by default in for..of.
+ * 
+ * 
+ * Set
+ * new Set(iterable) – creates the set, and if an iterable object is provided (usually an array), copies values from it into the set.
+ * set.add(value) – adds a value, returns the set itself.
+ * set.delete(value) – removes the value, returns true if value existed at the moment of the call, otherwise false.
+ * set.has(value) – returns true if the value exists in the set, otherwise false.
+ * set.clear() – removes everything from the set.
+ * set.size – is the elements count.
+ * 
+ * Iteration over Set
+ */
+
+
+
+// new Map()
+
+let map = new Map();
+map.set(1,  10)
+map.set(2, 20)
+map.set(3, 30)
+map.set(4, 40)
+map.set(5, 50)
+map.set(6, 60)
+map.set(7, 70)
+map.set(8, 80)
+
+// console.log(map.get(4));
+
+// console.log(map.has(5));
+
+// //console.log(map.delete(5));
+// console.log(map);
+
+//map.clear()
+// console.log(map);
+
+
+
+

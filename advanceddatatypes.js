cdc7584fdada547dde139e7a5977c6e7e55decdc
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-08-13 21:23:58 +0530

    entries

--- advanceddatatypes.js
+++ advanceddatatypes.js
@@ -374,6 +374,19 @@
   // Expected output: 42
   
 
+//   const object1 = {
+//     a: 'somestring',
+//     b: 42,
+//   };
+  
+//   for (const [key, value] of Object.entries(object1)) {
+//     console.log(`${key}: ${value}`);
+//   }
+//return the array of the given objects
+//   for(const [key, value] of Object.entries(object1)){
+//     console.log(`${key}: ${value}`)
+//   }
+  
 
 
 

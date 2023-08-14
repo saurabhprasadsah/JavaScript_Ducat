a93fa1d55aa7493ec349cbfd11a843a6a8293a01
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-08-14 20:36:33 +0530

    revisionfundamental.js

---
+++ fundamental/Revisionfundamental.js
@@ -0,0 +1,109 @@
+//console.log("hello world")
+// "use strict";
+
+/**
+ * 
+ * Code structure
+ * 
+ * Statements
+ * Semicolons
+ * Comments 
+*/
+//  console.log("hello world"); console.log("hello world")
+
+//  single line comments
+
+/**
+ * 
+ * multi line comments
+ * 
+ * 
+*/
+
+
+/**
+ * 
+ * The modern mode, "use strict"
+ * 
+ * “use strict”
+*/
+
+/**
+ * 
+ * Variables
+ * 
+ * A variable
+ * 
+ * let 
+ * const 
+ * var
+ * 
+ * Variable naming
+ * 
+ * The name must contain only letters, digits, or the symbols $ and _.
+ * The first character must not be a digit.
+*/
+
+
+// firstName ="Saurabh";
+// console.log(firstName);
+
+
+// //var
+// var firstName ="saurabh"
+// firstName ="JUHI"
+// console.log(firstName);
+
+// var firstName ="saurabh";
+
+// var PI =3.14;
+// var PI =4.34;
+// console.log(firstName);
+// console.log(PI);
+
+
+/*Data types
+ * 
+ * Number
+ * BigInt
+ * Boolean (logical type)
+ * null
+ * undefined
+ * array
+ * String
+ * Objects and Symbols
+ * 
+ */
+
+//integer type datatypes
+// let number = 65;
+// console.log(number);
+// console.log(typeof number);
+
+//BigINT types
+// let number = 444644416161611611111161161616161144161n;
+// console.log("Number", typeof number , number );
+
+// * Boolean (logical type)
+
+// let number = true;
+// console.log(typeof number);
+
+
+//undefiend
+// let number;
+// console.log(number);
+
+
+//array
+
+// let b=10;
+// let c =20;
+// let d =30;
+// let array =[b,c,d]
+
+// console.log (array);
+
+
+
+

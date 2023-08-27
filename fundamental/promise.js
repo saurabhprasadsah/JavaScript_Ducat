84303123ce8d1e1449407c07ee8249f9cd405691
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-08-27 10:33:53 +0530

    promise

--- fundamental/promise.js
+++ fundamental/promise.js
@@ -1,3 +1,18 @@
+
+//syntax
+// let promise =  new promise ((resolve, reject)=>{
+
+// })
+
+
+let item = ['pen','pencil']
 let promise =  new promise ((resolve, reject)=>{
-    
-})
+    if(item.includes('pen')){
+        resolve("item is availible")
+    }else{
+        reject("item not available")
+    }
+
+     })
+
+console.log(promise);

4a162de22f502ffa07b7db0c5a7ed123e89fc880
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-08-27 22:14:25 +0530

    intialcommot

--- fundamental/prototype.js
+++ fundamental/prototype.js
@@ -1,2 +1,79 @@
-let bool = true;
-console.log(bool.__proto__);
+
+// class test{
+
+// }
+
+// let t = new test();
+
+// console.log(t);
+
+// let array =[0,10]
+// console.log(array);
+
+
+//__proto__
+
+//Number
+
+// let number = 10;
+// console.log(number.__proto__);
+
+
+//string
+
+// let string ="dadasdas";
+// console.log(string.__proto__);
+
+
+// boolean
+// let bool = true;
+// console.log(bool.__proto__); // Boolean
+
+// //undefined
+// let num;
+// console.log(num.__proto__); // Error
+
+// null
+// let n = null;
+// console.log(n.__proto__); // Error
+
+//  array
+ // let array = [10,20,30];
+ // console.log(array.__proto__); // Array
+
+
+ //object 
+// let object = {
+//     firstName: 'John'
+// }
+// console.log(object.__proto__); // Object
+
+// symbol
+// let sym = Symbol('id')
+// console.log(sym.__proto__); // Symbol
+
+// BigInt
+// let bigint = 10n;
+// console.log(bigint.__proto__); // BigInt
+
+//prototype
+//  let array = [10,20,30]
+// // method
+//  Array.prototype.getName = function() {
+//      return  this.array }
+
+// property
+Array.prototype.name = "deepak singh gusain"
+
+console.log(array.name);
+
+
+
+
+
+
+
+
+
+// let bool = true;
+// console.log(bool.__proto__);

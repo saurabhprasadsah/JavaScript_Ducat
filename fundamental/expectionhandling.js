2d6db6d444c33877d0b4305034554ede60b7c1dc
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-02 12:17:35 +0530

    excetionhandling

--- fundamental/expectionhandling.js
+++ fundamental/expectionhandling.js
@@ -5,12 +5,30 @@
 //     console.log(error);
 // }
 
+// try{
+//     let b =2;
+//     let a =10/2;
+//     console.log(a);
+// }catch(error){
+//     console.log(error);
+// }finally{
+//     console.log("finally");
+// }
+
+//<!Custom Erorr -->
+
 try{
-    let b =2;
-    let a =10/2;
-    console.log(a);
+    let a =10;
+    let b =3;
+    if(!b){
+        throw new error("b value should be greater than zero");
+
+    }console.log(a/b);   
 }catch(error){
-    console.log(error);
-}finally{
-    console.log("finally");
+    console.log(error.message);
 }
+
+
+
+
+

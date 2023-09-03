b68a82400d1c986f6bf07f9f3dddb09c3db005a7
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-03 10:41:28 +0530

    adavancedfunction

--- fundamental/advanceFunction.js
+++ fundamental/advanceFunction.js
@@ -52,7 +52,54 @@ c
 
 /* hosting function--> call first the n it should be define as */
 // console.log(a);
-// a =10
+// a =10    
 
 
+//Rest parameters
+// function addition(...nums){
+//     let sum=0;
+//     for (const value of nums){
+//         sum+=value;
+//        }
+//        console.log(sum);
+// }
+// addition(10,50,30)
 
+
+//spread opeartor/Parameters
+
+
+// let array =[10,20,30,40,90];
+
+// console.log(array);
+// console.log(...array);
+
+// let array = [10,20,30,40,50];
+//spread operator
+// let array2 = [...array]
+// console.log(array);
+// console.log(array2);
+// array2.push(10000);
+// console.log(array);
+// console.log(array2);
+
+
+//objects
+let objects ={
+    firstName :"saurabh",
+    firstName:"raju", 
+}
+let obj2 = {...objects}
+obj2.age=10;
+objects.name="saurabh"
+console.log(objects);
+console.log(obj2);
+
+
+//objects
+// let objects ={
+//     first : "saurabh",
+//     first:"raju"
+// }
+
+// console.log(objects);

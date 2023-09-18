11229ea55dd1dc5f38945a15b8973228cb3a7fa3
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-18 16:33:34 +0530

    react

--- function.js
+++ function.js
@@ -1,47 +1,48 @@
-//function
+/*function
 
-// function declaration
-// local variables
+function declaration
+local variables
 
-// outer variables
-// parameters
-//default value
-//returning a values
-//function expression
-//call back function
-//arrow function
+outer variables
+parameters
+default value
+returning a values
+function expression
+call back function
+arrow function
 
-
-// function addition (){
-//     let a=10;
-//     let b = 20;
-//     console.log(a+b);
-
-// }
-
-// addition()
+*/
 
 
+/*function addition (){
+    let a=10;
+    let b = 20;
+    console.log(a+b);
 
-//pass the parametrs
-// function addition (num1,num2){
-    
-//     console.log(num1+num2);
-
-// }
-
-// addition(20,30)
+}
+*/
 
 
-//Nested function
-// function Testing() {
-//     console.log("outside test function")
-//     function test(){
-//         console.log("inside the funnction")
-//     }
-//     test()
-// }
-// Testing()
+
+
+/*addition()
+pass the parametrs
+function addition (num1,num2){
+    console.log(num1+num2);
+}
+addition(20,30)
+
+Nested function
+function Testing() {
+    console.log("outside test function")
+    function test(){
+        console.log("inside the funnction")
+    }
+    test()
+}
+Testing()
+
+*/
 
 
 // function  testing(){
@@ -53,78 +54,70 @@
 // console.log(a)
 
 
-//<------Global variables/global class --->
+/*<------Global variables/global class --->
 
-// let sum =0;
-// function addition(a,b){
-//     sum = a+b;
-// }
-// addition(10,20)
-// console.log(sum);
-// addition(10,20)
-
-
-// function expression
-
-// function()
-// {
-//     console.log("testing")
-// }
-
-// let addition = function(a,b){
-//     console.log(a,b);
-//     console.log("testing");
-// }
-
-// addition(10,20);
+let sum =0;
+function addition(a,b){
+    sum = a+b;
+}
+addition(10,20)
+console.log(sum);
+addition(10,20)
 
 
+function expression
 
-//<--------ARROW Function------->
+function()
+{
+    console.log("testing")
+}
 
-//SYNTAX OF ARROW FUNCTION
+let addition = function(a,b){
+    console.log(a,b);
+    console.log("testing");
+}
 
-// () => {
+addition(10,20);
 
-// }
+*/
 
-// let func = () =>{
-//     console.log(a+b);
-// }
+/*<--------ARROW Function------->
+
+SYNTAX OF ARROW FUNCTION
+
+() => {
+}
+
+let func = () =>{
+    console.log(a+b);
+}
+let addition = (a,b) => {
+    console.log(a+b);
+}
+addition(20,30)
 
 
-
-// let addition = (a,b) => {
-//     console.log(a+b);
-// }
-// addition(20,30)
-
-
-// let addition = function() {
-//     console.log(a+b);
-// }
-// addition(90,30)
+let addition = function() {
+    console.log(a+b);
+}
+addition(90,30)
 
 
 
-// let addition = (a,b) => a + b;
+let addition = (a,b) => a + b;
+let sum = addition(10,29);
+console.log(sum);
+callback function
+function calculate(a,b,func){
+    func(a,b)
+}
+calculate(10,20,function(){
 
-// let sum = addition(10,29);
+})
 
-// console.log(sum);
+console.log(10+20-10-0);
 
-
-//callback function
-
-// function calculate(a,b,func){
-//     func(a,b)
-// }
-// calculate(10,20,function(){
-
-
-// })
-
-// console.log(10+20-10-0);
+*/
 
 
 

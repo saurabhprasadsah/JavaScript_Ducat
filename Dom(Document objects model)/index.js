a2d56cb360ffc2ab4713f4edc3b77b77f6085d96
saurabhprasadsah <kumarsaurabh2910@gmail.com>
2023-09-23 22:25:29 +0530

    dom manipulation

--- Dom(Document objects model)/index.js
+++ Dom(Document objects model)/index.js
@@ -11,15 +11,121 @@
 
 // [meta] [title]       [h1] [h2]
 
-
-
-
-
 //Selectors
 //class Selectors
 //Tag/ elements
 
 //Query selectors
 //Query Selectors for all
+
+
+
+// selectors
+// id
+// let heading = document.getElementById('heading')
+
+// get data 
+// console.log(heading.innerText);
+// console.log(heading.innerHTML);
+
+// get set
+// heading.innerText = "Hello from DOM <button>Click Me!</button>";
+// heading.innerHTML = 
+// "Hello from DOM INNERHTML <button>Click Me!</button";
+
+// inline styling
+// heading.style.color = "white"
+// heading.style.backgroundColor = "red"
+
+// add class
+// heading.className = "red"
+
+// id
+// heading.id = "testing"
+
+// remove 
+// heading.remove()
+
+// class
+// let headings = document.getElementsByClassName('heading');
+
+// headings[0].className = "heading red";
+
+// for (const el of headings) {
+//     console.log(el);
+//     el.className = "heading black"
+// }
+
+
+// tag/element
+// let headings = document.getElementsByTagName('h1');
+
+// console.log(headings);
+
+// // headings[0].className = "heading red";
+// // headings[1].className = "heading red";
+
+// for (const el of headings) {
+//     console.log(el);
+//     el.className = "heading black"
+// }
+
+
+// querySelector
+// let heading = document.querySelector('#heading'); // id
+// let heading = document.querySelector('.heading'); // class
+// let heading = document.querySelector('h1'); // tag / element
+
+// console.log(heading);
+
+// querySelectorAll
+// let headings = document.querySelectorAll('#heading'); // id
+// let headings = document.querySelectorAll('.heading'); // class
+// let headings = document.querySelectorAll('h1'); // tag / element
+// console.log(headings);
+
+// create elements
+// let div = document.createElement('div');
+// div.innerText = "Div from js";
+// div.className = "red"
+
+// let root = document.getElementById('root');
+
+// append / appendChild add element at the last 
+// root.append(div)
+// root.appendChild(div)
+
+// prepend add element at the first
+// root.prepend(div)
+
+// traversing elements
+// parent
+// let paragraph = document.getElementById('paragraph');
+// console.dir(paragraph.parentElement);
+// console.dir(paragraph.parentNode);
+// console.log(paragraph.parentElement.parentElement.parentElement.parentElement);
+// console.log(paragraph.parentNode.parentNode.parentNode.parentNode);
+
+// children
+// let list = document.getElementById('list');
+// console.log(list.childNodes);
+// console.log(list.children);
+
+// siblings
+// previous sibling
+// let list = document.getElementById('list');
+// console.log(list.previousSibling);
+// console.log(list.previousElementSibling);
+// console.log(list.previousElementSibling.previousElementSibling.previousElementSibling);
+
+// next sibling
+let list = document.getElementById('list');
+console.log(list);
+// console.log(list.nextSibling);
+// console.log(list.nextElementSibling);
+// console.log(list.nextElementSibling.nextElementSibling.nextElementSibling.nextElementSibling);
+
+console.log(navigator.language)
+
 
 
--- fundamental/genratorfunction.js
+++ fundamental/genratorfunction.js
@@ -2,11 +2,12 @@
 function* genratorfunction (a,b,c){
     yield a; 
     return a    // if return the value then it should not be excute the next value!
-    yield a+10;
+    yield a+ 10;
     yield a +30; 
     yield b +30; 
     yield c + 30;  
 }
+
 let gen = genratorfunction(10,20,30);
 console.log('next',gen.next()); // {value: 10, done: false }
 console.log("next",gen.next()); //{ value : undefined ,done : true };
